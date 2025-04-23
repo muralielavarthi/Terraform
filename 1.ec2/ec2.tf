a38573cf20dba4a7a16263c94532d506d9bba8ae
Murali Elavarthi <muralis2p@gmail.com>
2025-04-23 18:17:05 +0530

    c1

--- 1.ec2/ec2.tf
+++ 1.ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "this" {
-  ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
+  ami                    = "ami-09c813fb71547fc4f"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   instance_type          = "t3.micro"
   tags = {
@@ -7,26 +7,40 @@ resource "aws_instance" "this" {
     Purpose = "terraform-practice"
   }
 }
+/* 
+this - this name is for terraform reference
+terraform-demo - this name we will appear for ec2-instance
+tags are usefull for filteration purpose.
+*/
 
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls_1"
   description = "Allow TLS inbound traffic and all outbound traffic"
 
-  ingress {
+  #inbound traffic
+  ingress { 
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ #outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "-1" # -1: all protocols
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "allow_tls" # in Name - N is uppercase in tags
   }
 }
+
+/* 
+
+resource "resource-type" "resource-name" {
+    key= value pairs
+}
+*/
