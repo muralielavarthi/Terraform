f3f981c85f897c332d27ef2b846af111b77e47c6
Murali Elavarthi <muralis2p@gmail.com>
2025-04-24 10:45:11 +0530

    c1

--- 9.for-each/locals.tf
+++ 9.for-each/locals.tf
@@ -1,1 +1,4 @@
-
+locals {
+  record_name=each.key == "frontend" ? var.domain_name : "${each.key}.${var.domain_name}"
+  records=each.key == "frontend" ? [each.value.public_ip] : [each.value.private_ip]
+}
--- 9.for-each/r53.tf
+++ 9.for-each/r53.tf
@@ -1,9 +1,9 @@
 resource "aws_route53_record" "expense" {
-  for_each = aws_instance.this
+  for_each = aws_instance.this # copying output to for_each (aws_instance.this, have output of instance created)
   zone_id = var.zone_id
-  name    = each.key == "frontend" ? var.domain_name : "${each.key}.${var.domain_name}"
+  name    = local.record_name
   type    = "A"
   ttl     = 1
-  records = each.key == "frontend" ? [each.value.public_ip] : [each.value.private_ip]
+  records = local.records
   allow_overwrite = true
 }
